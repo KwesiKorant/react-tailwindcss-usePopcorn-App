1edf47f66bc825c1ebdfd5d68d6215e77ab9385c
Vade <junykorant@gmail.com>
2026-04-16 22:31:17 +0000

    4th commit - made the StarRating component reusable.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-// import './styles.css'
-// import App from './App.jsx'
+import { StrictMode} from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import './styles.css'
+import App from './App.jsx'
+// import StarRating from "./StarRating.jsx";
 
-import StarRating from './StarRating.jsx'
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       {" "}
+//       <StarRating
+//         messages={["Terrible", "Bad", "Meh", "Good", "Great"]}
+//         defaultRating={0}
+//         maxRating={10}
+//         onSetRating={setMovieRating}
+//       />
+//       <h1>This movie was rated {movieRating} stars</h1>
+//     </div>
+//   );
+// }
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <StarRating/>
+    <App />
+    {/* <StarRating
+      messages={["Terrible", "Bad", "Meh", "Good", "Great"]}
+      defaultRating={3}
+    />
+    <Test /> */}
   </StrictMode>,
-)
+);
